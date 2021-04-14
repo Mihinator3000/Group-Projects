3b603702feab9cc85d9cb44c0737db370292e145
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2021-04-14 13:59:06 +0300

    Something

---
+++ DeadInside/film_table.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class FilmTable:
+    def __init__(self, database_file):
+        self.connection = sqlite3.connect(database_file)
+        self.cursor = self.connection.cursor()
+
+    def get_film(self):
+        with self.connection:
+            return self.cursor.execute("SELECT * FROM `Users` WHERE `id` >= 0").fetchall()
+
+    def film_exists(self, name_rus):
+        with self.connection:
+            result = self.cursor.execute("SELECT * FROM `Films` WHERE `name_rus` = ?", (name_rus,)).fetchall()
+            return bool(len(result))
+
+    def add_film(self, name_rus, link):
+        with self.connection:
+            return self.cursor.execute("INSERT INTO `Films` (`film_name_rus`, `link`) VALUES (?,?)", (name_rus, link))
+
+    def update_link(self, name_rus, link):
+        return self.cursor.execute("UPDATE `Films` SET `link` = ? WHERE `name_rus` = ?", link, name_rus)
+
+    def close(self):
+        self.connection.close()
---
+++ DeadInside/user_table.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class UserTable:
+    def __init__(self, database_file):
+        self.connection = sqlite3.connect(database_file)
+        self.cursor = self.connection.cursor()
+
+    def get_subscriptions(self, status=True):
+        with self.connection:
+            return self.cursor.execute("SELECT * FROM `Users` WHERE `status_of_subscription` = ?", (status,)).fetchall()
+
+    def subscriber_exists(self, user_id):
+        with self.connection:
+            result = self.cursor.execute("SELECT * FROM `Users` WHERE `user_id` = ?", (user_id,)).fetchall()
+            return bool(len(result))
+
+    def add_subscriber(self, user_id, status=True):
+        with self.connection:
+            return self.cursor.execute("INSERT INTO `Users` (`user_id`, `status_of_subscription`) VALUES (?,?)", (user_id, status))
+
+    def update_subscription(self, user_id, status):
+        return self.cursor.execute("UPDATE `Users` SET `status_of_subscription` = ? WHERE `user_id` = ?", (status, user_id))
+
+    def close(self):
+        self.connection.close()
