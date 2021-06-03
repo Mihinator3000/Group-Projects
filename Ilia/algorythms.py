706e818ee0ec978068bb55654413aa528a3edcfb
wilfordaf <furyfire200@yandex.ru>
2021-06-03 16:49:23 +0300

    alpha v0.01

---
+++ Ilia/algorythms.py
@@ -0,0 +1,39 @@
+import Dheap
+
+
+INF = 100000000001
+
+
+class Edge:
+    def __init__(self, startNode_, endNode_, length_):
+        self.startNode = startNode_
+        self.endNode = endNode_
+        self.length = length_
+
+
+def dijkstra(s, numOfNodes, d, used, availableWays, lengthOfWays):
+    d[s - 1] = 0
+    for i in range(numOfNodes):
+        v = -1
+        for j in range(numOfNodes):
+            if not used[j] and (v == - 1 or d[j] < d[v]):
+                v = j
+        if d[v] == INF:
+            break
+        used[v] = True
+        for j in range(availableWays[v].length()):
+            if d[availableWays[v][j]] > d[v] + lengthOfWays[v][j]:
+                d[availableWays[v][j]] = d[v] + lengthOfWays[v][j]
+
+
+def fold_bellman(s, d, Edges):
+    d[s] = 0
+    while True:
+        any = False
+        for edge in Edges:
+            if d[edge.startNode] < INF:
+                if d[edge.endNode] > d[edge.startNode] + edge.length:
+                    d[edge.endNode] = d[edge.startNode] + edge.length
+                    any = True
+        if not any:
+            break
