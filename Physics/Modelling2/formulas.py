c851dd0b6c2e6f1e52c20a953aaa127a816c844c
wilfordaf <furyfire200@yandex.ru>
2021-06-08 19:17:51 +0300

    Final epoch

--- Physics/Modelling2/formulas.py
+++ Physics/Modelling2/formulas.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Globals
 e = -1.6 * 1 ** (-19)
 m = 9.1 * 1 ** (-31)
-frequency = 10**6
+delta_t = 10**6
 
 
 def calculate_a(y, U, R, r):
@@ -15,21 +15,19 @@ def calculate_a(y, U, R, r):
 def graph(U, R, r, l, v0):
     r_inner = R - r
     v = [v0]
-    v_x = [v0]
-    v_y = [0]
+    v_x, v_y = [v0], [0]
     a_y = [0]
-    x = [0]
-    y = [r_inner + r/2]
+    x, y = [0], [r_inner + r/2]
     t = [0]
     dt = 1
     while x[-1] < l and r_inner < y[-1] < R:
         v.append(math.sqrt(v_x[-1] ** 2 + v_y[-1] ** 2))
-        t.append(dt / frequency)
+        t.append(dt / delta_t)
         a_y.append(calculate_a(y[-1], U, R, r))
-        v_y.append(a_y[-1] * dt / frequency)
+        v_y.append(a_y[-1] * dt)
         v_x.append(v0)
-        y.append(r_inner + r/2 + (a_y[-1] * dt**2)/(2 * frequency))
-        x.append(v0 * dt / frequency)
+        y.append(r_inner + r/2 + (a_y[-1] * dt**2)/(2 * delta_t))
+        x.append(v0 * dt / delta_t)
 
         dt += 1
 
@@ -50,6 +48,6 @@ def graph(U, R, r, l, v0):
     plt.plot(t, v)
     plt.subplot(2, 2, 4)
     plt.title("a(t)")
-    plt.plot(t, a_y)
+    plt.plot(t[1::-1], a_y[1::-1])
     plt.show()
 
