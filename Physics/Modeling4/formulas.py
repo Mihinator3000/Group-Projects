e6bf7c0457591fa9eb9f65f38be2b797a85c0bbb
wilfordaf <furyfire200@yandex.ru>
2021-12-22 10:08:41 +0300

    fix: diff equation solution & formulas

--- Physics/Modeling4/formulas.py
+++ Physics/Modeling4/formulas.py
@@ -2,29 +2,33 @@ from math import *
 from matplotlib import pyplot as plt
 
 
-def calculate_x(t, C1, C2, a):
-    return C1 * t - ((C2 * exp(-1 * a**2 * t)) / a**2)
+def calculate_x(t, C1, C2, C3, a):
+    return C1 + C2 * cos(2 * t / a) + C3 * sin(2 * t / a)
 
 
-def calculate_y(t, C2, a):
-    return C2 * exp(-1 * a**2 * t) / a
+def calculate_y(t, C2, C3, C4, a):
+    return C4 + C3 * (2 / a**2) * cos(2 * t / a) - C2 * (2 / a**2) * sin(2 * t / a)
 
 
 def make_graph(m, q, v, B, alpha, color: str):
     a = q * B / m
-    C1 = (v * (a * cos(alpha) + sin(alpha))) / a
-    C2 = -1 * (v * sin(alpha)) / a
+
+    C1 = (v * sin(alpha) * a**3) / 4
+    C2 = -1 * C1
+    C3 = (v * cos(alpha)) / 2
+    C4 = (-1 * v * cos(alpha)) / a
 
     arr_x = []
     arr_y = []
-    arr_t = [i / 10**14 for i in range(2 * 10**5)]
+    arr_t = [i for i in range(10**5)]
 
-    for t in arr_t:
-        arr_x.append(calculate_x(t, C1, C2, a))
-        arr_y.append(calculate_y(t, C2, a))
+    for num, t in enumerate(arr_t):
+        arr_x.append(calculate_x(t, C1, C2, C3, a))
+        arr_y.append(calculate_y(t, C2, C3, C4, a))
+        if arr_y[-1] < -0.05:
+            arr_t = arr_t[0:num]
+            break
 
-    min_y = abs(min(arr_y))
-    arr_y_normalized = [y + min_y for y in arr_y]
-    plt.plot(arr_x, arr_y_normalized, color=color)
+    plt.plot(arr_x, arr_y, color=color)
     plt.title("y(x)")
     plt.show()
