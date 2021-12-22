a7733a4bbbdf277e7635bbd3d11c14781d121cca
wilfordaf <furyfire200@yandex.ru>
2021-12-22 14:26:32 +0300

    fix: again...

--- Physics/Modeling4/formulas.py
+++ Physics/Modeling4/formulas.py
@@ -3,32 +3,33 @@ from matplotlib import pyplot as plt
 
 
 def calculate_x(t, C1, C2, C3, a):
-    return C1 + C2 * cos(2 * t / a) + C3 * sin(2 * t / a)
+    return C1 + C2 * cos(a * t) + C3 * sin(a * t)
 
 
 def calculate_y(t, C2, C3, C4, a):
-    return C4 + C3 * (2 / a**2) * cos(2 * t / a) - C2 * (2 / a**2) * sin(2 * t / a)
+    return C4 + C3 * cos(a * t) - C2 * sin(a * t)
 
 
 def make_graph(m, q, v, B, alpha, color: str):
     a = q * B / m
 
-    C1 = (v * sin(alpha) * a**3) / 4
+    C1 = (v * sin(alpha)) / a
     C2 = -1 * C1
-    C3 = (v * cos(alpha)) / 2
-    C4 = (-1 * v * cos(alpha)) / a
+    C3 = (v * cos(alpha)) / a
+    C4 = -1 * C3
 
     arr_x = []
     arr_y = []
-    arr_t = [i for i in range(10**5)]
+    arr_t = [i / 10**6 for i in range(10**6)]
 
     for num, t in enumerate(arr_t):
         arr_x.append(calculate_x(t, C1, C2, C3, a))
         arr_y.append(calculate_y(t, C2, C3, C4, a))
-        if arr_y[-1] < -0.05:
+        if arr_y[-1] < 0:
             arr_t = arr_t[0:num]
             break
 
     plt.plot(arr_x, arr_y, color=color)
     plt.title("y(x)")
+    # plt.ylim(0, 0.03)
     plt.show()
