a50cc6966ccf30bc86151a7487631e2994f943db
Sergey <79761555+wilfordaf@users.noreply.github.com>
2022-10-04 10:43:07 +0300

    test: added test case for maximization

--- PriMat/lab5/lab1_tests.py
+++ PriMat/lab5/lab1_tests.py
@@ -58,6 +58,32 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(-13, func_extremum)
         self.assertTrue(np.array_equal(np.array([3, 2]), extremum_coordinates))
 
+    def test_case_3(self):
+        r_1 = r.Restriction(np.array([3, 2, 5]), r.RestrictionType.LESS, 18)
+        r_2 = r.Restriction(np.array([4, 2, 3]), r.RestrictionType.LESS, 16)
+        r_3 = r.Restriction(np.array([2, 1, 1]), r.RestrictionType.GREATER, 4)
+        statement = st.Statement(np.array([3, 2, 4]), [r_1, r_2, r_3])
+        solution_aim = ssm.SolutionAim.MAX
+
+        func_vector, b, vectors, value = statement.create_statement()
+        basis = statement.create_basis()
+        basis_content = statement.create_basis_content()
+
+        simplex_solution_method = ssm.SimplexSolutionMethod(
+            solution_aim,
+            func_vector,
+            b,
+            vectors,
+            basis,
+            basis_content)
+
+        func_extremum, extremum_coordinates = simplex_solution_method.solve(value)
+        print(f'Function extremum: {func_extremum:.3f}')
+        print(f'Extremum coordinates: {extremum_coordinates}')
+
+        self.assertEqual(17, func_extremum)
+        self.assertTrue(np.array_equal([0, 6.5, 1], extremum_coordinates))
+
 
 if __name__ == '__main__':
     unittest.main()
